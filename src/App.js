dce24c4bbae4aece4e7d32c797972f0f1782c398
AxSch <alex_schuneman@outlook.com>
2020-07-25 19:40:56 +0100

    Add Switch & Post route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 import './App.css';
 import Posts from './containers/posts/Posts';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import Post from './containers/post/Post';
 
 
 function App() {
   return (
     <Router>
+      <Switch>
         <Route exact path="/">
           <Redirect to="r/pics" />
         </Route>
         <Route exact path="/r/pics" component={Posts} />
+        <Route path={`/r/pics/:postid`} component={Post} />
+      </Switch>
     </Router>
   );
 }
