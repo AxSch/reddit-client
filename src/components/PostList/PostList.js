cf9ef7b5911818a4b89c2f48c430b0b670f659d4
AxSch <alex_schuneman@outlook.com>
2020-07-25 23:04:45 +0100

    Refactor component & render based on posts.length

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -8,6 +8,7 @@ const PostList = () => {
     const posts = useSelector(selectPosts)
     const dispatch = useDispatch()
     const nextPageString = useSelector(selectNextPage)
+
     const renderPosts = posts => {
         if (posts) {
             return posts.map((post, key) => {
@@ -19,17 +20,27 @@ const PostList = () => {
             return ""
         }
     }
-    const fetchMorePosts = async() => {
+    const fetchMorePosts = async () => {
         const posts = await fetchAPI.fetchPosts(nextPageString)
         dispatch(updatePosts(posts.data.children))
     }
 
+    const renderPostsSection = (posts) => {
+        if (posts.length > 0) {
+            return (
+                <>
+                    {renderPosts(posts)}
+                    <div>
+                        <button onClick={fetchMorePosts}>Show More</button>
+                    </div>
+                </>
+            )
+        } else return ""
+    }
+
     return (
         <div>
-            {renderPosts(posts)}
-            <div>
-                <button onClick={fetchMorePosts}>Show More</button>
-            </div>
+            {renderPostsSection(posts)}
         </div>
     )
 }
