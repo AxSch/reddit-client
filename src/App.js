df693723918304793f665fbefe64d97a36de4af9
AxSch <alex_schuneman@outlook.com>
2020-07-25 23:05:16 +0100

    Add route lazy loading

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 import './App.css';
-import Posts from './containers/posts/Posts';
-import Post from './containers/post/Post';
+import SkeletalLoading from './components/SkeletalLoading';
 
+const Posts = lazy(() => import('./containers/posts/Posts'))
+const Post = lazy(() => import('./containers/post/Post'))
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Redirect to="r/pics" />
-        </Route>
-        <Route exact path="/r/pics" component={Posts} />
-        <Route path={`/r/pics/:postid`} component={Post} />
-      </Switch>
-    </Router>
+    <div>
+      <Suspense fallback={<SkeletalLoading />}>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="r/pics" />
+            </Route>
+            <Route exact path="/r/pics" component={Posts} />
+            <Route path={`/r/pics/:postid`} component={Post} />
+          </Switch>
+        </Router>
+      </Suspense>
+    </div>
   );
 }
 
