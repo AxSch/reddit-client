64ec5b2b27f31774d2e922fbd7e6f0f30dd9a98f
AxSch <alex_schuneman@outlook.com>
2020-07-26 02:03:43 +0100

    Refactor to include renderHeader()

--- src/containers/post/Post.js
+++ src/containers/post/Post.js
@@ -2,6 +2,8 @@ import React, { Component, lazy, Suspense } from 'react'
 import { fetchAPI } from '../../api/api'
 import SkeletalLoading from '../../components/SkeletalLoading'
 import ErrorBoundary from '../../ErrorBoundary'
+import { selectPost } from '../../reducers/posts/postsSlice'
+import store from '../../app/store'
 
 const CommentList = lazy(() => import('../../components/CommentList/CommentList'))
 
@@ -11,6 +13,7 @@ class Post extends Component {
         
         this.state = {
             postComments: null,
+            post: null
         }
     }
 
@@ -28,16 +31,38 @@ class Post extends Component {
     }
     
     componentDidMount() {
+        const { location } = this.props
+        const splitLocation = location.pathname.split('/')
+        const postId = splitLocation[splitLocation.length - 1]
+
         this.fetchComments() 
+        const post = selectPost(store.getState(), postId)
+        this.setState({post: post})
+    }
+
+    renderHeader(post) {
+        if (post && post.length > 0) {
+            return (
+                <div>
+                    <div>
+                        <h1>{post[0].title}</h1>
+                    </div>
+                    <div>
+                        <img src={post[0].url} alt={post[0].author_flair_text} width="" height="" />
+                    </div>
+                </div>
+            )
+        } else return ""
     }
     
     render() {
-        const { postComments } = this.state
+        const { postComments, post } = this.state
 
         return (
             <div>
                 <ErrorBoundary>
                     <Suspense fallback={<SkeletalLoading type={"post"} />}>
+                        {this.renderHeader(post)}
                         <CommentList comments={postComments ? postComments.children: []} />
                     </Suspense>
                 </ErrorBoundary>
