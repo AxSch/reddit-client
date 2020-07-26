d6fa7bdc75dcc544e46c6792dfee44917037af50
AxSch <alex_schuneman@outlook.com>
2020-07-26 02:05:17 +0100

    Refactor CommentList to include renderFn()'s
    Also added calculateTopComments()

--- src/components/CommentList/CommentList.js
+++ src/components/CommentList/CommentList.js
@@ -1,26 +1,66 @@
 import React from 'react'
 
 const CommentList = ({ comments }) => {
-    const renderComments = (comments) => {
-        return comments.map((comment, key) => {
+    const calculateTopComments = comments => {
+        const topComments = []
+        let prevScore = 0
+        let maxScore = 0
+        if (comments.length > 0) {
+            comments.forEach(comment => {
+                if (comment.data.score > prevScore) {
+                    prevScore = comment.data.score
+                    if (prevScore > maxScore) {
+                        maxScore = prevScore
+                        topComments.push(comment.data)
+                    }
+                }
+
+            })
+        }
+        return topComments
+    }
+
+    const renderComments = comments => {
+        return comments.reverse().map((comment, key) => {
             return (
                 <div key={key}>
-                    Comment {key}
+                    <div>
+                        {comment.author}
+                        {comment.score}
+                        {comment.created}
+                    </div>
+                    <div>
+                        {comment.body}
+                    </div>
                 </div>
+
             )
         })
-        
+
     }
-    
+
+    const renderCommentsSection = comments => {
+        if (comments !== [] && comments.length > 0) {
+            const noOftopComments = calculateTopComments(comments).length
+            const topComments = calculateTopComments(comments)
+            const renderTopComments = renderComments(topComments)
+
+            return (
+                <>
+                    <div>Top comments {noOftopComments} </div>
+                    {renderTopComments}
+                </>
+            )
+
+        } else {
+            return ""
+        }
+    }
+
+
     return (
         <div>
-            <div>
-                header
-            </div>
-            <div>
-                IMG
-            </div>
-            {renderComments(comments)}
+            {renderCommentsSection(comments)}
         </div>
     )
 }
