480131831befb8e39514c48fe4f4e0a4c0389c72
AxSch <alex_schuneman@outlook.com>
2020-07-27 18:52:54 +0100

    Refactor normalizeData() & added filterData()

--- src/helpers/normalizeData.js
+++ src/helpers/normalizeData.js
@@ -1,12 +1,26 @@
-export const normalizeData = (data) => {
+const filterData = data => {
     const filteredData = data.filter(element => element.data["over_18"] === false)
+    if (filteredData.length > 0) {
+        return filterData
+    } else return data
+        
+}
+
+export const normalizeData = data => {
+    const filteredData = filterData(data)
     const normalizedData = []
     let dataObj = {}
+    const flatData = {}
+
     filteredData.forEach(element => {
         dataObj = element.data
         normalizedData.push(dataObj)
 
     })
-    return normalizedData
+
+    normalizedData.forEach(item => {
+        flatData[item.id] = item
+    })
+    return flatData
 
 }
