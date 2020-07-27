e34e419ccdb04bbd4e73f851a9e86f6789eb06f0
AxSch <alex_schuneman@outlook.com>
2020-07-27 23:39:34 +0100

    Implement skeleton loading

--- src/components/SkeletalLoading.js
+++ src/components/SkeletalLoading.js
@@ -1,19 +1,39 @@
 import React from 'react'
+import './SkeletalLoading.scss'
 
-const SkeletalLoading = (type) => {
-    if (type === 'posts') {
+const SkeletalLoading = ({ type }) => {
+    const renderPostsSkeleton = (amount) => {
+        const postCards = []
+        for (let i = 0; i < amount; i++) {
+            postCards.push(<div className="postlist-card-dummy" key={i} />)
+        }
+        return postCards
+
+    }
+
+    const renderBlock = (amount) => {
+        const skeletonCards = renderPostsSkeleton(amount)
         return (
-            <div>
-                Loading Posts...
-            </div>
-        )
-    } else {
-        return (
-            <div>
-                Loading Post...
-            </div>
+            <>
+                <div className="posts-header-dummy" />
+                <div className="postlist-col">
+                    {skeletonCards}
+                </div>
+            </>
         )
     }
+    
+    switch (type) {
+        case "post":
+            return renderBlock(3)
+        case "posts":
+            return renderBlock(6)
+        case "comments":
+            return renderBlock(2)
+        default:
+            return ""
+    }
+
 }
 
 export default SkeletalLoading
