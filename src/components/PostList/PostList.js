0b61789dda63ec61c393cfc60a79cc56cec62356
AxSch <alex_schuneman@outlook.com>
2020-07-28 03:04:31 +0100

    Refactor component, making it simpler
    Removed fetchMorePosts() as this is now being handled by the Posts
    container

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { selectPosts, selectNextPage, updatePosts, setNextPage } from '../../reducers/posts/postsSlice'
+import { useSelector } from 'react-redux'
+import { selectPosts } from '../../reducers/posts/postsSlice'
 import PostListItem from '../PostListItem/PostListItem'
-import { fetchAPI } from '../../api/api'
-import './PostList.scss'
+import SkeletalLoading from '../SkeletalLoading/SkeletalLoading'
 
 const PostList = () => {
     const posts = useSelector(selectPosts)
-    const dispatch = useDispatch()
-    const nextPageString = useSelector(selectNextPage)
 
     const renderPosts = posts => {
         if (posts) {
@@ -21,21 +18,13 @@ const PostList = () => {
             return ""
         }
     }
-    
-    const fetchMorePosts = async () => {
-        const posts = await fetchAPI.fetchPosts(nextPageString)
-        dispatch(updatePosts(posts.data.children))
-        dispatch(setNextPage(posts.data.after))
-    }
 
     const renderPostsSection = (posts) => {
         if (Object.keys(posts).length > 0) {
             return (
                 <>
                     {renderPosts(posts)}
-                    <div className="postlist-row">
-                        <button className="postlist-button" onClick={fetchMorePosts}>Show More</button>
-                    </div>
+                    <SkeletalLoading type="posts-ext" />
                 </>
             )
         } else return ""
