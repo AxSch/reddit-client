2cf370147fd6fd425d527531a42087058469c01c
AxSch <alex_schuneman@outlook.com>
2020-07-28 17:48:51 +0100

    Add getPostAwards() & renderAwards()
    Also styled accordingly

--- src/containers/post/Post.js
+++ src/containers/post/Post.js
@@ -48,13 +48,13 @@ class Post extends Component {
         this.setState({ post: post })
     }
 
-    checkHasImage (post) {
+    checkHasImage(post) {
         if (post.hasOwnProperty('preview')) {
             return (
                 <img className="post-img" src={post.url} alt={post.title} />
             )
         } else {
-            const postRAW = ReactHtmlParser(post.selftext_html, {decodeEntities:true})
+            const postRAW = ReactHtmlParser(post.selftext_html, { decodeEntities: true })
             const cleanedHTML = postRAW[0].split('<!-- SC_ON -->')
             const cleanedText = cleanedHTML[0].split('<!-- SC_OFF -->')
             const htmlText = cleanedText[1]
@@ -68,14 +68,60 @@ class Post extends Component {
         }
     }
 
+    getPostAwards(post) {
+        if (post.all_awardings && post.all_awardings.length > 0) {
+            const countedAwards = {}
+
+            const awards = post.all_awardings.map(item => {
+                const awardObj = {
+                    name: "",
+                    desc: "",
+                    url: "",
+                }
+                awardObj.name = item.name
+                awardObj.desc = item.description
+                awardObj.url = item.icon_url
+                return awardObj
+            })
+
+            Object.values(awards).forEach(award => {
+                const awardCategoryObj = {
+                    ...award,
+                    count: 1
+                }
+                if (countedAwards.hasOwnProperty(award.name)) {
+                    countedAwards[award.name]["count"] += 1
+                } else {
+                    countedAwards[award.name] = awardCategoryObj
+                }
+            })
+            return countedAwards
+        }
+    }
+
+    renderAwards(awards) {
+        if(awards) {
+            return Object.values(awards).map((award, key) => {
+                return (
+                    <div className="post-awards-icons" key={key}>
+                        <img src={award.url} alt={award.desc} width="24px" height="24px" />
+                        {award.count > 1 ? <span>{award.count}</span> : ""}
+                    </div>
+                )
+            })
+        } else return ""
+    }
+
     renderHeader(post) {
         const { location, history } = this.props
         const splitLocation = location.pathname.split('/')
-        const previousPath = splitLocation.splice(1, splitLocation.length -2).join("/")
+        const previousPath = splitLocation.splice(1, splitLocation.length - 2).join("/")
 
         if (post) {
+            const postAwards = this.getPostAwards(post)
             this.checkHasImage(post)
             const postDate = formatDate(post.created)
+
             return (
                 <div>
                     <div>
@@ -90,9 +136,12 @@ class Post extends Component {
                         <div className="post-header-row">
                             <span>submitted on {postDate.pDate} at {postDate.pTime} by <span className="post-author">{post.author}</span></span>
                         </div>
+                        <div className="post-awards-row">
+                            {this.renderAwards(postAwards)}
+                        </div>
                         <div className="post-header-row-actions">
-                            <span><FontAwesomeIcon className="postlist-icon" icon={faBomb}/>{parseInt(post.score).toLocaleString()}</span>
-                            <span><FontAwesomeIcon className="postlist-icon" icon={faCommentAlt}/>{parseInt(post.num_comments).toLocaleString()} comments</span>
+                            <span><FontAwesomeIcon className="postlist-icon" icon={faBomb} />{parseInt(post.score).toLocaleString()}</span>
+                            <span><FontAwesomeIcon className="postlist-icon" icon={faCommentAlt} />{parseInt(post.num_comments).toLocaleString()} comments</span>
                             <span> share </span>
                             <span>save </span>
                             <span> hide </span>
@@ -112,13 +161,13 @@ class Post extends Component {
     render() {
         const { post, isLoading } = this.state
         let renderComments
-        
+
         if (isLoading) {
             renderComments = <SkeletalLoading type="comments" />
         } else {
             renderComments = <CommentList />
         }
-        
+
         return (
             <div className="post">
                 <ErrorBoundary>
